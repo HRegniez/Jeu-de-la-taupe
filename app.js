168e7acd35c0e8db8f78b15d4e4a86968dd5b226
HRegniez <henriregniez124@gmail.com>
2022-09-06 10:11:43 +0200

    New proj

---
+++ app.js
@@ -0,0 +1,4 @@
+const carte = document.querySelectorAll('.carte');
+const taupe =querySelector('.taupe');
+const tempRestant = document.querySelector('#t-temps');
+const points = document.querySelector('#points');
